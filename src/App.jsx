1a00285fe1d82ba3da326bccfed264167b4cb8e4
MagicSleet <angelicomatias@gmail.com>
2026-05-15 12:06:33 -0600

    Update App.jsx
    ortografía

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
       <section id="objetivo" className="my-4">
         <div className="p-4 rounded" style={{backgroundColor:'#000', color:'#fff'}}>
           <h2>¿Para que lo hacemos?</h2>
-          <p>Usando la API publica de (TheMealDB). Este sitio muestra recetas, permite una busqueda, y contiene una sección de contacto ideal para familias o gente que quiere probar algo nuevo.</p>
+          <p>Usando la API pública de TheMealDB, este sitio muestra recetas, permite realizar búsquedas y contiene una sección de contacto; ideal para familias o gente que quiere probar algo nuevo.</p>
         </div>
       </section>
 
@@ -80,7 +80,7 @@ const App = () => {
       <section id="sobre-nosotros" className="my-4">
         <div className="p-4 rounded" style={{backgroundColor:'#000', color:'#fff'}}>
           <h2>Sobre nosotros</h2>
-          <p> Soy Matias A. M. y esta pagina se creeo para el primer parcial de programacion en internet con el objetivo de ayudar a familias y gente que quiere probar recetas nuevas.</p>
+          <p> Soy Matías A. M. y esta página se creó para el primer parcial de Programación en Internet, con el objetivo de ayudar a familias y gente que quiera probar recetas nuevas.</p>
         </div>
       </section>
 
